c6c03994cdcc8f64f9e9f26232730df88f537967
OmarTellez1 <94090584+OmarTellez1@users.noreply.github.com>
2025-12-15 01:03:03 +0000

    Chore: Apply automated code style fixes

--- src/admin/modelos/asistencia_marcacion.php
+++ src/admin/modelos/asistencia_marcacion.php
@@ -1,52 +1,52 @@
-<?php 
+<?php
+
 //incluir la conexion de base de datos
 require "../config/Conexion.php";
-class Asistencia{
+class Asistencia
+{
+    //implementamos nuestro constructor
+    public function __construct()
+    {
+    }
 
 
-	//implementamos nuestro constructor
-public function __construct(){
 
-}
+    public function verificarcodigo_persona($codigo_persona)
+    {
+        $sql = "SELECT * FROM usuarios WHERE codigo_persona='$codigo_persona'";
+        return ejecutarConsultaSimpleFila($sql);
+    }
 
+    public function seleccionarcodigo_persona($codigo_persona)
+    {
+        $sql = "SELECT * FROM asistencia WHERE codigo_persona = '$codigo_persona'";
+        return ejecutarConsulta($sql);
+    }
 
+    public function registrar_entrada($codigo_persona, $tipo)
+    {
+        date_default_timezone_set('America/Lima');
+        $fecha = date("Y-m-d");
+        $hora = date("H:i:s");
+        $sql = "INSERT INTO asistencia (codigo_persona,  tipo, fecha) VALUES ('$codigo_persona', '$tipo', '$fecha')";
+        return ejecutarConsulta($sql);
+    }
 
-public function verificarcodigo_persona($codigo_persona){
- 	$sql = "SELECT * FROM usuarios WHERE codigo_persona='$codigo_persona'";
-	return ejecutarConsultaSimpleFila($sql);
-}
-
-public function seleccionarcodigo_persona($codigo_persona){
-    $sql = "SELECT * FROM asistencia WHERE codigo_persona = '$codigo_persona'";
-	return ejecutarConsulta($sql);
-}
-
-public function registrar_entrada($codigo_persona,$tipo){
-	date_default_timezone_set('America/Lima');
-	$fecha = date("Y-m-d");
-	$hora = date("H:i:s");
-    $sql = "INSERT INTO asistencia (codigo_persona,  tipo, fecha) VALUES ('$codigo_persona', '$tipo', '$fecha')";
-	return ejecutarConsulta($sql);
-}
-
-public function registrar_salida($codigo_persona,$tipo){
-	date_default_timezone_set('America/Lima');
-	$fecha = date("Y-m-d");
-	$hora = date("H:i:s");
- 	$sql = "INSERT INTO asistencia (codigo_persona,  tipo, fecha) VALUES ('$codigo_persona', '$tipo', '$fecha')";
-    return ejecutarConsulta($sql);
-}
+    public function registrar_salida($codigo_persona, $tipo)
+    {
+        date_default_timezone_set('America/Lima');
+        $fecha = date("Y-m-d");
+        $hora = date("H:i:s");
+        $sql = "INSERT INTO asistencia (codigo_persona,  tipo, fecha) VALUES ('$codigo_persona', '$tipo', '$fecha')";
+        return ejecutarConsulta($sql);
+    }
 
 
 
 //listar registros
-public function listar(){
-	$sql="SELECT * FROM asistencia";
-	return ejecutarConsulta($sql);
+    public function listar()
+    {
+        $sql = "SELECT * FROM asistencia";
+        return ejecutarConsulta($sql);
+    }
 }
-
-
-}
-
- ?>
-
